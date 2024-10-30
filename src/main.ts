6b06aacca93ff8fb590b34df82c32b00dde592e4
moonstream-labs <ryan@moonstream.xyz>
2024-10-30 03:41:49 -0400

    🚀

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 import nuxtUI from '@nuxt/ui/vue-plugin'
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/main.css'
 
 const app = createApp(App)
 // ...
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
+// vite.config.ts
+
 import ui from '@nuxt/ui/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        ui(),
-        vueDevTools(),
-    ],
+  plugins: [
+    vue(),
+    ui({
+      ui: {
+        colors: {
+          primary: 'green',
+          neutral: 'zinc',
+        },
+      },
+    }),
+    vueDevTools(),
+  ],
 })
